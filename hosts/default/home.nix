df68350cef1fb44e05ae28b6450c043cf95e7609
Kimbix <alemahumberto06@gmail.com>
2024-05-20 13:33:30 -0400

    Added git.nix

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -18,6 +18,7 @@
 		../../modules/home-manager/shells/starship.nix
 		../../modules/home-manager/note-taking/obsidian.nix
 		../../modules/home-manager/development/godot.nix
+		../../modules/home-manager/development/git.nix
 	];
 
 	widgets.ags.enable = true;
