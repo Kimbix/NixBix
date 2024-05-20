93857a5ceeca46205f726fbffa088938078a8d35
Kimbix <alemahumberto06@gmail.com>
2024-05-20 13:39:23 -0400

    Testing

--- modules/home-manager/development/git.nix
+++ modules/home-manager/development/git.nix
@@ -1,7 +1,7 @@
 { ... }: {
   programs.git.enable = true;
   programs.git.aliases = {
-    user-kimbix = "config user.name \"Kimbix\"; git config user.email \"alemanhumberto06@gmail.com\"";
-    user-uni = "config user.name \"HumbertoAleman\"; git config user.email \"haleman.23@est.ucab.edu.ve\"";
+    user-kimbix = "config user.name \"Kimbix\" && git config user.email \"alemanhumberto06@gmail.com\"";
+    user-uni = "config user.name \"HumbertoAleman\" && git config user.email \"haleman.23@est.ucab.edu.ve\"";
   };
 }
